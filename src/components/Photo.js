f4c8b4707605199bf3d000c60c8d86bc263b23e3
Owner <owner@Users-MacBook-Pro.local>
2020-02-09 19:26:24 -0500

    added photo in the container for 'yesterdays photo'

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -6,12 +6,21 @@ const Image = styled.img`
   min-width: 200px;
   max-height: 600px;
   object-fit: cover;
+  margin: 20px;
 `;
 
 function Photo(props) {
   return (
     <div className="main-photo-container">
-      <Image src={props.img} alt="photo of the day" />
+      <div className="image-container">
+        <Image src={props.img} alt="photo of the day" />
+        <p> Todays Photo</p>
+      </div>
+
+      <div className="image-container">
+        <Image src="https://spacenews.com/wp-content/uploads/2019/07/Artemis-Lander-concept-art.jpg" />
+        <p>Yesterdays photo</p>
+      </div>
     </div>
   );
 }
